a5254c05a9558156020b9565df91b8ed4bdd6b5e
이윤슬 <ysisys0202@gmail.com>
2025-02-12 22:06:10 +0900

    💄 Pagination 추가

---
+++ src/components/pagination/pagination.tsx
@@ -0,0 +1,27 @@
+import { Button } from "chak-blocks/plain";
+import React from "react";
+import { paginationStyles } from "./style.css";
+
+type Props = {
+  pageLength: number;
+  currentPage: number;
+  className?: string;
+};
+
+const Pagination = ({ pageLength, currentPage, className }: Props) => {
+  return (
+    <div className={`${paginationStyles.self} ${className}`}>
+      {Array.from({ length: pageLength }, (_, index) => (
+        <Button
+          key={index + 1}
+          variant={index + 1 === currentPage ? "contained" : "outlined"}
+          theme="dark"
+        >
+          {index + 1}
+        </Button>
+      ))}
+    </div>
+  );
+};
+
+export default Pagination;
---
+++ src/components/pagination/style.css.ts
@@ -0,0 +1,10 @@
+import { style } from "@vanilla-extract/css";
+
+export const paginationStyles = {
+  self: style({
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 8,
+  }),
+};
