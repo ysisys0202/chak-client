6e36fbbb95b702d840eeb0ad0e316bd1649e92b3
이윤슬 <ysisys0202@gmail.com>
2025-02-18 22:51:36 +0900

    🔧 react query client retry false 설정

--- src/providers/query.tsx
+++ src/providers/query.tsx
@@ -4,7 +4,13 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const QueryProvider = ({ children }: { children: React.ReactNode }) => {
-  const queryClient = new QueryClient();
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+      },
+    },
+  });
 
   return (
     <QueryClientProvider client={queryClient}>
