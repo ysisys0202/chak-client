7004d1b31e8911c3d92d1609fb2c32d2c94cd9ea
이윤슬 <ysisys0202@gmail.com>
2025-02-23 04:29:49 +0900

    ✨  RecordDetailButtonGroup API 연결

--- src/components/record-table/record-detail-button-group.tsx
+++ src/components/record-table/record-detail-button-group.tsx
@@ -1,17 +1,28 @@
-import { Button } from "chak-blocks/plain";
-import ButtonGroup from "./button-group";
+"use client";
+
 import Link from "next/link";
+import { useParams } from "next/navigation";
+import { Button } from "chak-blocks/plain";
 import path from "@/constants/path";
+import { useDeleteRecordMutation } from "@/query/record";
+import ButtonGroup from "./button-group";
 
 const RecordDetailButtonGroup = ({ className }: { className?: string }) => {
+  const { id } = useParams();
+  const { mutate } = useDeleteRecordMutation();
+
+  const handleDelete = () => {
+    confirm("삭제된 기록은 복구할 수 없습니다. 정말 삭제하시겠습니까?");
+    mutate(id as unknown as number);
+  };
   return (
     <ButtonGroup className={className}>
-      <Link href={`${path.recordEdit}/1`}>
+      <Link href={`${path.recordEdit}/${id}`}>
         <Button theme="primary" as="div">
           수정하기
         </Button>
       </Link>
-      <Button theme="error" variant="outlined">
+      <Button theme="error" variant="outlined" onClick={handleDelete}>
         삭제하기
       </Button>
     </ButtonGroup>
