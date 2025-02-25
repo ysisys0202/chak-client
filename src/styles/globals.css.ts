d7d4d6febea5794298cc566b2764661fe5ae1361
이윤슬 <ysisys0202@gmail.com>
2025-02-25 17:39:55 +0900

    💄 전역 스타일 수정

--- src/styles/globals.css.ts
+++ src/styles/globals.css.ts
@@ -2,5 +2,5 @@ import { globalStyle } from "@vanilla-extract/css";
 import { gray } from "@/constants/colors";
 
 globalStyle("html", {
-  backgroundColor: gray[50],
+  backgroundColor: "#fffbf3",
 });
