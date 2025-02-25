cb7304284c3629a12f834343b461c865b3c7340e
이윤슬 <ysisys0202@gmail.com>
2025-02-25 16:21:22 +0900

    🐛 HomeSection className prop 추가

--- src/components/sections/shared/section.tsx
+++ src/components/sections/shared/section.tsx
@@ -1,5 +1,10 @@
-const HomeSection = ({ children }: { children: React.ReactNode }) => {
-  return <section>{children}</section>;
+import { combineClassNames } from "@/util/common";
+
+type Props = { className?: string; children: React.ReactNode };
+
+const HomeSection = ({ className, children }: Props) => {
+  const classNames = combineClassNames(className);
+  return <section className={classNames}>{children}</section>;
 };
 
 export default HomeSection;
