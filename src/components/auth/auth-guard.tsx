f72bf22abc87b8a4ce0d13ad7ee3b4980fe44dc6
이윤슬 <ysisys0202@gmail.com>
2025-03-02 13:50:09 +0900

    🐛 🚧   auth 디버깅 중

--- src/components/auth/auth-guard.tsx
+++ src/components/auth/auth-guard.tsx
@@ -1,10 +1,18 @@
 import { getToken } from "@/util/auth";
 import { getAuth } from "@/api/auth";
-
+import { headers } from "next/headers";
 const AuthGuard = async ({ children }: { children: React.ReactNode }) => {
+  const headersList = await headers();
+  console.log("auth guard headerList", headersList);
+  const token = headersList
+    .get("cookie")
+    ?.split(";")
+    .find((c) => c.trim().startsWith("token="))
+    ?.split("=")[1];
+  console.log("token", token);
   try {
     const token = await getToken();
-    console.log("token :", token);
+    console.log("auth guard :", token);
     await getAuth({
       headers: {
         Authorization: `Bearer ${token}`,
