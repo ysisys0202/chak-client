0e4f507cc610907850710db65e91cc51c2f71f2e
이윤슬 <ysisys0202@gmail.com>
2025-03-02 10:36:59 +0900

    ♻️ AuthGuard 서버 컴포넌트로 수정

--- src/components/auth/auth-guard.tsx
+++ src/components/auth/auth-guard.tsx
@@ -1,28 +1,22 @@
-"use client";
-
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-import { useToast } from "chak-blocks/context";
+import { redirect } from "next/navigation";
 import path from "@/constants/path";
-import { useAuth } from "@/providers/auth";
-import FullScreenLoader from "@/components/shared/loading/full-screen-loader";
+import { getToken } from "@/util/auth";
+import { getAuth } from "@/api/auth";
 
-const AuthGuard = ({ children }: { children: React.ReactNode }) => {
-  const { open } = useToast();
-  const router = useRouter();
-  const { user, isAuthenticated, isAuthenticating } = useAuth();
+const AuthGuard = async ({ children }: { children: React.ReactNode }) => {
+  try {
+    const token = await getToken();
+    await getAuth({
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
 
-  // TODO : useToast open 함수를 useEffect의 dependency array에 추가하면 무한 렌더링 이슈 발생, chak-blocks 확인 필요
-  useEffect(() => {
-    if (!isAuthenticating && !isAuthenticated) {
-      open({ title: "로그인 후에 사용해주세요.", status: "error" });
-      router.replace(path.login);
-    }
-  }, [user, isAuthenticating, isAuthenticated]);
-  if (isAuthenticating) {
-    return <FullScreenLoader />;
+    return <>{children}</>;
+  } catch (error) {
+    console.error(error);
+    redirect(path.login);
   }
-  return <>{children}</>;
 };
 
 export default AuthGuard;
